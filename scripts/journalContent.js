00c3d929579b9de94a2422aee965b02f38552859
cslunetta <cslunetta@outlook.com>
2020-12-10 15:11:36 -0600

    moved hardcoded entry data to .json file for flexibility

--- scripts/journalContent.js
+++ scripts/journalContent.js
@@ -1,13 +1,15 @@
-import { useJournalEntries } from "./journalDataProvider.js"
-import { journalEntry } from "./journalEntry.js"
+import { useJournalEntries, getJournalEntries } from "./journalDataProvider.js";
+import { journalEntry } from "./journalEntry.js";
+
+// refrence element to add code after
+const contentElement = document.querySelector(".journalContent");
 
 export const journalContent = () => {
-  // refrence element to add code after
-  const contentElement = document.querySelector(".journalContent");
-  const journalData = useJournalEntries()
-
-  for (const journalObject of journalData) {
-    const journalHTML = journalEntry(journalObject);
-    contentElement.innerHTML += journalHTML;
-  }
-}
+  getJournalEntries().then(() => {
+    let journalEntries = useJournalEntries();
+    for (const journalObject of journalEntries) {
+      const journalHTML = journalEntry(journalObject);
+      contentElement.innerHTML += journalHTML;
+    }
+  });
+};
